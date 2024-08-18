d4b2942ac8fee335dd4c16f0d14a04c8450900ce
Florian Marending <florian@marending.dev>
2024-08-18 23:12:16 +0200

    Add test script

---
+++ ts-sqlite/src/test-upload.ts
@@ -0,0 +1,20 @@
+import { faker } from "@faker-js/faker";
+
+const response = await fetch("http://localhost:3000", {
+  method: "POST",
+  body: JSON.stringify([
+    {
+      data: {
+        lux: faker.number.float({ min: 0, max: 100_000 }),
+      },
+      timestamp: new Date().toISOString(),
+    },
+  ]),
+  headers: { "Content-Type": "application/json" },
+});
+
+if (response.status === 200) {
+  console.log("OK 200");
+} else {
+  console.log(response);
+}
