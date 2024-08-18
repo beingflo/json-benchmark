20b00bc5a4c644deb6b3910356b83687234a985f
Florian Marending <florian@marending.dev>
2024-08-18 15:56:19 +0200

    Continue building ts sqlite

--- ts-sqlite/src/index.ts
+++ ts-sqlite/src/index.ts
@@ -1,7 +1,35 @@
 import { Elysia } from "elysia";
+import { Database } from "bun:sqlite";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const db = new Database("./db.sqlite");
+db.run(`
+  CREATE TABLE IF NOT EXISTS metrics (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      timestamp TEXT NOT NULL,
+      data TEXT NOT NULL
+  );
+`);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+db.run("PRAGMA journal_mode = WAL;");
+
+const app = new Elysia()
+  .get("/", () => {
+    const query = db.query("SELECT * FROM metrics;");
+    const results = query.all();
+
+    return results;
+  })
+  .post("/", () => {
+    const query = db.query(
+      "INSERT INTO metrics (timestamp, data) VALUES ($timestamp, $data)"
+    );
+    const results = query.run({
+      $timestamp: new Date().toISOString(),
+      $data: `{ "test": 123 }`,
+    });
+
+    return results;
+  })
+  .listen(3000);
+
+console.log(`Running at ${app.server?.hostname}:${app.server?.port}`);
