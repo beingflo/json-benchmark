ea52741c4e9dc9c4f390d70b0b910cc72b6c5890
Florian Marending <florian@marending.dev>
2024-08-25 22:12:00 +0200

    Add sqlite virtual table

--- benchmarks/insert.js
+++ benchmarks/insert.js
@@ -10,8 +10,8 @@ export default function () {
   const res = http.post(
     "http://localhost:3000/",
     JSON.stringify({
-      data: { temperature: 12.3, humidity: 52.8 },
-      type: "humidity",
+      data: { temperature: 12.3, co2: 2 },
+      type: "co2",
       timestamp: new Date().toISOString(),
     }),
     { headers: { "Content-Type": "application/json" } }
---
+++ ts-sqlite/src/index-virtual.ts
@@ -0,0 +1,113 @@
+import { Elysia, t } from "elysia";
+import { Database } from "bun:sqlite";
+
+const db = new Database("./db-virtual.sqlite");
+db.run(`
+  CREATE TABLE IF NOT EXISTS metrics (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      timestamp TEXT NOT NULL,
+      data TEXT NOT NULL
+  );
+`);
+
+try {
+  db.run(`
+  ALTER TABLE metrics 
+  ADD COLUMN co2 INTEGER 
+  AS (json_extract(data, '$.co2'));
+`);
+} catch (e) {
+  console.log("co2 column already exists");
+}
+
+try {
+  db.run(`
+  ALTER TABLE metrics 
+  ADD COLUMN humidity REAL
+  AS (json_extract(data, '$.humidity'));
+`);
+} catch (e) {
+  console.log("humidity column already exists");
+}
+
+try {
+  db.run(`
+  ALTER TABLE metrics 
+  ADD COLUMN month TEXT 
+  AS (strftime('%m', timestamp));
+`);
+} catch (e) {
+  console.log("month column already exists");
+}
+
+db.run(`CREATE INDEX IF NOT EXISTS co2_index on metrics(co2);`);
+db.run(`CREATE INDEX IF NOT EXISTS humidity_index on metrics(humidity);`);
+db.run(`CREATE INDEX IF NOT EXISTS month_index on metrics(month);`);
+db.run(
+  `CREATE INDEX IF NOT EXISTS month_humidity_index on metrics(month, humidity);`
+);
+
+db.run("PRAGMA journal_mode = WAL;");
+db.run("PRAGMA synchronous = normal;");
+
+const app = new Elysia()
+  .onError(({ code, error }) => {
+    console.log(error.message);
+    return code;
+  })
+  .get("/", ({ query }) => {
+    const qry = db.query("SELECT * FROM metrics;");
+    const results = qry.all();
+
+    return results;
+  })
+  .get("/co2-high", ({ query }) => {
+    // const qry = db.query(
+    //   "SELECT count(*) as count FROM metrics WHERE data ->> '$.co2' > 1990;"
+    // );
+    const qry = db.query(
+      "SELECT count(*) as count FROM metrics WHERE co2 > 0;"
+    );
+    const results = qry.get() as { count: number };
+
+    return results.count;
+  })
+  .get("/humidity-avg", ({ query }) => {
+    //const qry = db.query(
+    //  "SELECT timestamp, avg(data ->> '$.humidity') as avg FROM metrics WHERE data ->> '$.humidity' GROUP BY strftime('%m', timestamp);"
+    //);
+    const qry = db.query(
+      "SELECT month, avg(humidity) as avg FROM metrics GROUP BY month;"
+    );
+    const results = qry.all();
+
+    return results;
+  })
+  .post("/delete", () => {
+    const query = db.query("DELETE FROM metrics");
+    query.run();
+  })
+  .post(
+    "/",
+    ({ body }) => {
+      const query = db.query(
+        "INSERT INTO metrics (timestamp, data) VALUES ($timestamp, $data)"
+      );
+
+      const result = query.run({
+        $timestamp: body.timestamp ?? new Date().toISOString(),
+        $data: JSON.stringify(body.data),
+      });
+
+      return 200;
+    },
+    {
+      body: t.Object({
+        data: t.Any(),
+        timestamp: t.Optional(t.String({ format: "date-time" })),
+      }),
+    }
+  )
+  .listen(3000);
+
+console.log(`Running at ${app.server?.hostname}:${app.server?.port}`);
