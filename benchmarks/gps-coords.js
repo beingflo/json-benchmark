54a7db8938d6b0d9ccf44981da0806ca188656b9
Florian Marending <florian@marending.dev>
2024-09-03 21:49:44 +0200

    Add gps coordinates benchmark

---
+++ benchmarks/gps-coords.js
@@ -0,0 +1,14 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = {
+  vus: 10,
+  duration: "5s",
+};
+
+export default function () {
+  const res = http.get("http://localhost:3007/gps/coordinates");
+
+  check(res, { "status was 200": (r) => r.status == 200 });
+  check(res, { "response was correct": (r) => r.body == "2787" });
+}
