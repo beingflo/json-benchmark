048e0fbf133666ddc497d84145a1d8e9f9e330a0
Florian Marending <florian@marending.dev>
2024-09-05 21:48:38 +0200

    Add rust-sqlite

---
+++ rust-sqlite/src/migration.rs
@@ -0,0 +1,21 @@
+use rusqlite::Connection;
+use rusqlite_migration::{Migrations, M};
+
+pub struct Entry {
+    pub bucket: String,
+    pub date: String,
+    pub data: String,
+}
+
+pub fn apply_migrations(connection: &mut Connection) {
+    let migrations = Migrations::new(vec![M::up(
+        "CREATE TABLE IF NOT EXISTS metrics (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            timestamp TEXT NOT NULL,
+            data TEXT NOT NULL
+        );",
+        "CREATE INDEX IF NOT EXISTS timestamp_index on metrics(timestamp);",
+    )]);
+
+    migrations.to_latest(connection).unwrap();
+}
